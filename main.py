4e7f089b7bc94345c86a62add185eda66f1ef7d8
Heinz <heinz.hscharlach@gmail.com>
2025-09-12 23:18:30 +0200

    Updated LLM response

--- main.py
+++ main.py
@@ -23,41 +23,38 @@ def main():
     messages = [# To be used Later to store the entire conversation with the LLM
         types.Content(role="user", parts=[types.Part(text=input)]),
     ]
-    final_response = ""
-    #try:
-    for i in range(20):
-        response = client.models.generate_content(
-            model='gemini-2.0-flash-001', 
-            contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions],
-                system_instruction=system_prompt,
-                ),
-            )
-        for candidate in response.candidates:
-            messages.append(candidate.content)
-        #if not response.function_calls:
-            #return response.text
-        for function_call_part in response.function_calls:
-            function_call_result = call_function(function_call_part, verbose)
-            if function_call_result.parts[0].function_response.response:
-                #print(types.Part(text=f"-> {function_call_result.parts[0].function_response.response['result']}"))
-                messages.append(types.Content(role='user', parts=[types.Part(text=function_call_result.parts[0].function_response.response['result'])]))
+    try:
+        for i in range(20):
+            response = client.models.generate_content(
+                model='gemini-2.0-flash-001', 
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt,
+                    ),
+                )
+            if response.candidates:
+                print(response.candidates)
+                for candidate in response.candidates:
+                    messages.append(candidate.content)
+            if response.text:
+                print("Final response:")
+                print(response.text)
+                break
+            elif response.function_calls:
+                print(response.function_calls)
+                for function_call_part in response.function_calls:
+                    function_call_result = call_function(function_call_part, verbose)
+                    if function_call_result.parts[0].function_response.response:
+                        messages.append(types.Content(role='user', parts=[types.Part(text=function_call_result.parts[0].function_response.response['result'])]))
+                    else:
+                        raise Exception("Error: Fatal error when calling function.")
             else:
-                raise Exception("Error: Fatal error when calling function.")
-        #print(f"-> {function_call_result.parts[0].function_response.response['result']}")
-        if response.text:
-            #final_response = response.text
-            break
-    if verbose:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
-    #print(f"-> {function_call_result.parts[0].function_response.response}")
-    #print(final_response)
-    print("testing")
-    #print(messages[3].parts)
-    #print(messages[len(messages) - 1].parts[0].text)
-    """except Exception as e:
-        print(f"Error: Fatal error as {e}")"""
+                break
+        if verbose:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}")
+    except Exception as e:
+        print(f"Error: Fatal error as {e}")
 
 if __name__ == "__main__":
     main()
